8f6e1971346880c86263d1051f7903faa6dfd2e4
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-05 23:26:08 +0300

    feat: add model for start page

---
+++ src/scripts/start_page/Model/Model.js
@@ -0,0 +1,54 @@
+import {
+  ViewStartPage
+} from '../View/index';
+import {
+  APIClass
+} from '../../../API/index';
+import {
+  setLocalStorageData
+} from '../../../LocalStorage/index';
+
+const ViewSP = new ViewStartPage()
+const API = new APIClass()
+
+export const ModelStartPage = class {
+
+  init() {
+    ViewSP.viewHeaderStartPage();
+  }
+
+  onChangeForm(value) {
+    switch (value) {
+      case 'register':
+        ViewSP.viewStartPage(value);
+        break;
+      case 'login':
+        ViewSP.viewStartPage(value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  onRegisterUser(login, password, date_birth, sex) {
+      API.createNewUser(login, password, date_birth, sex).then(data => {
+        if (data.regStatus) {
+          localStorage.clear()
+          setLocalStorageData('id_user', data.id_user);
+        } else {
+          ViewSP.onErrorValue('register-login')
+        }
+      })
+  }
+
+  onLoginUser(login, password) {
+    API.loginUser(login, password).then(data => {
+      if (data.loginStatus) {
+        localStorage.clear()
+        setLocalStorageData('id_user', data.id_user);
+      } else {
+        ViewSP.onErrorValue('login-value')
+      }
+    })
+  }
+}
