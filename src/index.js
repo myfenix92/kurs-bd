a6c88dcc6682f3c7bbc997bc495f80d9b6e7a005
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-17 01:13:44 +0300

    feat: add import style, add behavior for reload page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 //import './styles/start_page/style.css';
 //import './styles/main_page/style.css';
-import './styles/table-page/style.css'
+import './styles/style.css'
 import { ControllerStartPage } from './scripts/start_page/Controller/index';
 import { ModelStartPage } from './scripts/start_page/Model/index';
 import { ControllerMainPage } from './scripts/main_page/Controller/index'
 import { ControllerTablePage } from './scripts/table-page/Controller/index'
 import { ModelMainPage } from './scripts/main_page/Model/index'
 import { ModelTablePage } from './scripts/table-page/Model/index';
-import { getLocalStorageData } from './LocalStorage';
+import { getLocalStorageData, setLocalStorageData } from './LocalStorage';
 
 const ControllerSP = new ControllerStartPage()
 const ModelSP = new ModelStartPage()
@@ -37,6 +37,8 @@ document.body.addEventListener('click', ControllerTP.onFilterHandler)
 document.body.addEventListener('click', ControllerTP.onDeleteStickerHandler)
 document.body.addEventListener('click', ControllerTP.onChangeNameStickerHandler)
 document.body.addEventListener('click', ControllerTP.onChangeRecordHandler)
+document.body.addEventListener('click', ControllerTP.onHistoryChangesHandler)
+document.body.addEventListener('click', ControllerTP.onDeleteTable)
 document.body.addEventListener('keyup', ControllerTP.onChangeRecordHandler)
 document.body.addEventListener('keyup', ControllerTP.onChangeNameStickerHandler)
 document.body.addEventListener('keyup', ControllerTP.onCreateRecord)
@@ -46,15 +48,13 @@ document.body.addEventListener('keyup', ControllerTP.onChangeNameTableHandler)
 document.body.addEventListener('change', ControllerTP.onChangeIsDoneRecord)
 
 //ModelSP.init()
+//setLocalStorageData('id_user', 6)
+//ModelTP.getStickers(147)
 
-ModelTP.getStickers(17)
-// function authCheck() {
-//   if (!getLocalStorageData('token')) {
-//     ModelSP.init()
-//   } else {
-//     ModelMP.init()
-//   }
-
-// }
-
-// authCheck()
+document.addEventListener("DOMContentLoaded", function(){
+  if (!getLocalStorageData('token')) {
+        ModelSP.init()
+      } else {
+        ModelMP.init()
+      }
+ })
