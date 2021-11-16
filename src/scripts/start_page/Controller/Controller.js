cc2e925e14b693a4365e1954d06464e748b1c105
Yana Kornejkova <bloom.hog@gmail.com>
2021-11-17 01:17:05 +0300

    feat: add check for login, change text to russian

--- src/scripts/start_page/Controller/Controller.js
+++ src/scripts/start_page/Controller/Controller.js
@@ -1,8 +1,16 @@
-import { ModelStartPage } from "../Model/index";
-import { ModelMainPage } from "../../main_page/Model";
+import {
+  ModelStartPage
+} from "../Model/index";
+import {
+  ModelMainPage
+} from "../../main_page/Model/index";
+import {
+  ViewStartPage
+} from "../View/index";
 
 const ModelSP = new ModelStartPage()
 const ModelMP = new ModelMainPage()
+const ViewSP = new ViewStartPage()
 
 export const ControllerStartPage = class {
   constructor() {
@@ -11,9 +19,9 @@ export const ControllerStartPage = class {
 
   onClickHandler(event) {
     if (event.target.classList.contains('header_btn_register') && event.target.tagName === 'BUTTON') {
-        this.clickValue = 'register';
-        ModelSP.onChangeForm(this.clickValue)
-    } else 
+      this.clickValue = 'register';
+      ModelSP.onChangeForm(this.clickValue)
+    } else
     if (event.target.classList.contains('header_btn_log') && event.target.tagName === 'BUTTON') {
       this.clickValue = 'login';
       ModelSP.onChangeForm(this.clickValue)
@@ -21,52 +29,51 @@ export const ControllerStartPage = class {
   }
 
   onContinueHandler(event) {
-
     this.errorPassword = document.querySelector('.error_password.password');
     this.errorPasswordCheck = document.querySelector('.error_password.password_check');
-      this.login = document.querySelector('#login');
-      this.password = document.querySelector('#password');
-      this.passwordCheck = document.querySelector('#password_check');
-      this.date_birth = document.querySelector('#b-day');
-      this.sex = Array.from(document.getElementsByName('sex'));
+    this.login = document.querySelector('#login');
+    this.password = document.querySelector('#password');
+    this.passwordCheck = document.querySelector('#password_check');
+    this.date_birth = document.querySelector('#b-day');
+    this.sex = Array.from(document.getElementsByName('sex'));
     if (this.clickValue === 'register') {
-       if (event.target.classList.contains('form_in_continue') && event.target.tagName === 'BUTTON') {
+      if (event.target.classList.contains('form_in_continue') && event.target.tagName === 'BUTTON') {
         document.querySelectorAll('.error_password').forEach((e) => {
           e.textContent = '';
         })
         if (this.password.value === this.passwordCheck.value &&
           (this.password.value.length < 6 || this.password.value.length > 20)) {
-            this.errorPasswordCheck.textContent = '';
-          this.errorPassword.textContent = 'Length less 6 or more 20 symbols';
+          this.errorPasswordCheck.textContent = '';
+          this.errorPassword.textContent = 'Длина меньше 6 или 20 символов';
         } else if (this.password.value !== this.passwordCheck.value) {
           this.errorPasswordCheck.textContent = '';
-          this.errorPasswordCheck.textContent = 'Password mismatch';
+          this.errorPasswordCheck.textContent = 'Пароли не совпадают';
         } else if (document.querySelector('#b-day').value === '') {
           this.errorPassword = document.querySelector('.error_password.b_day');
-          this.errorPassword.textContent = 'Input your b-day';
-        }
-        else if (this.password.value === this.passwordCheck.value) {
-          ModelSP.onRegisterUser(this.login.value, this.password.value, this.passwordCheck.value, this.date_birth.value, this.sex.find(r => r.checked).value);
+          this.errorPassword.textContent = 'Введите дату рождения';
+        } else if (this.password.value === this.passwordCheck.value) {
+          ModelSP.onRegisterUser(this.login.value, this.password.value, this.date_birth.value, this.sex.find(r => r.checked).value);
           document.querySelector('header').innerHTML = '';
-          document.querySelector('header').innerHTML = `<div class="header"></div>`;
           document.querySelector('main').innerHTML = '';
           ModelMP.init();
         }
 
-       }
+      }
     }
 
     if (this.clickValue === 'login') {
       if (event.target.classList.contains('form_in_continue') && event.target.tagName === 'BUTTON') {
         ModelSP.onLoginUser(this.login.value, +this.password.value);
-        document.querySelector('header').innerHTML = '';
-        document.querySelector('header').innerHTML = `<div class="header"></div>`;
-        document.querySelector('main').innerHTML = '';
-        ModelMP.init()
-       
+        setTimeout(() => {
+          if (localStorage.length !== 0) {
+            document.querySelector('header').innerHTML = '';
+            document.querySelector('main').innerHTML = '';
+            ModelMP.init()
+          }
+        }, 300)
+
       }
-    }
-    
-};
+    } 
+  };
 
 }
