1a95f2d904f4578b0c8eb7f6f5a9b7906d70e385
Yana Korneykova <bloom.hog@gmail.com>
2022-11-08 19:36:21 +0300

    feat: add msg from users

--- src/scripts/admin_page/Model/Model.js
+++ src/scripts/admin_page/Model/Model.js
@@ -9,19 +9,39 @@ import { getLocalStorageData, getIdUser } from '../../../LocalStorage';
 
 const ViewAP = new ViewAdminPage()
 const API = new APIClass()
+let info_users = []
 export const ModelAdminPage = class {
 
   init() {
     this.onGetUsers();
+    this.onGetCountUsers();
+  }
+
+  onGetCountUsers() {
+    API.getCountUsers().then(data => {
+      console.log(data)
+      data.forEach((el) => {
+        ViewAP.viewAdminCountUsers(el.all_users, el.online)
+      })
+    })
   }
 
   onGetUsers() {
+    info_users = [];
+    API.getMsgFromUsers().then(data => {
+        data.forEach((el) => {
+          info_users.push(el);
+        })
+        console.log(info_users)
+    })
+   
     API.getUsers().then(data => {
-      console.log(data)
       ViewAP.ViewCreateTable();
       data.forEach((el) => {
-        ViewAP.viewAdminPage(el.id_user, el.login, el.sex, el.date_birth.slice(0, 10), el.date_registr.slice(0, 10), el.count_tables)
+        ViewAP.viewAdminPage(el.id_user, el.login, el.sex, el.date_birth.slice(0, 10), el.date_registr.slice(0, 10), el.count_tables,
+        info_users.find(count_msg => count_msg.id_user === el.id_user).count_msg)
       })
     })
+    
   }
 }
