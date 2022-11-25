ef881eb4836f9f6cf06d63ba24114dfee0d4d0fc
Yana Korneykova <bloom.hog@gmail.com>
2022-11-25 23:18:24 +0300

    fix: change get user

--- src/LocalStorage/LocalStorage.js
+++ src/LocalStorage/LocalStorage.js
@@ -19,12 +19,14 @@ export const setLocalStorageData = (key, data) => {
 }
 
 export function getIdUser() {
-  let token = getLocalStorageData('token');
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-//  let id_user = JSON.parse(jsonPayload)
-  return JSON.parse(jsonPayload).id_user;
+  if (getLocalStorageData('token')) {
+    let token = getLocalStorageData('token');
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+  //  let id_user = JSON.parse(jsonPayload)
+    return JSON.parse(jsonPayload).id_user;
+  }
 }
